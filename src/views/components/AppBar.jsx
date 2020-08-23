b3df4b45300376a57933193f98a64cad426b4d70
alsanusi <malkautsars@gmail.com>
2020-08-24 01:29:14 +0800

    fix: contact form

--- src/views/components/AppBar.jsx
+++ src/views/components/AppBar.jsx
@@ -45,7 +45,12 @@ const useStyles = makeStyles((theme) => ({
         fontWeight: 'bold',
         border: '1px solid #C12121',
         margin: theme.spacing(1, 1.5)
-    }
+    },
+    formBase: {
+        border: '2px solid #8a2a2a',
+        borderRadius: 20,
+        boxShadow: '-9px 9px 20px rgb(51 51 51 / 17%), -10px 8px 40px rgb(232 232 232 / 23%)'
+    },
 }));
 
 export default function Pricing() {
@@ -63,7 +68,7 @@ export default function Pricing() {
     return (
         <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
             <CssBaseline />
-            <ContactDialog isOpen={openDialog} isClose={handleCloseDialog} />
+            <ContactDialog isOpen={openDialog} isClose={handleCloseDialog} className={classes.formBase} />
             <Toolbar className={classes.toolbar}>
                 <Toolbar className={classes.toolbarTitle}>
                     <Link component={NavLink} to={'/'}>
--- src/views/components/ContactDialog.jsx
+++ src/views/components/ContactDialog.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -6,27 +6,63 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+import Grid from '@material-ui/core/Grid';
+// import { makeStyles } from '@material-ui/core/styles';
+
+const Transition = forwardRef(function Transition(props, ref) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+// const useStyles = makeStyles((theme) => ({
+//     cardBase: {
+//         border: '2px solid #8a2a2a',
+//         borderRadius: 20,
+//         boxShadow: '-9px 9px 20px rgb(51 51 51 / 17%), -10px 8px 40px rgb(232 232 232 / 23%)'
+//     },
+// }));
+
 
 export default function FormDialog({ ...props }) {
     const { isOpen, isClose } = props
+    // const classes = useStyles();
 
     return (
         <div>
-            <Dialog open={isOpen} onClose={isClose} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+            <Dialog open={isOpen} onClose={isClose} TransitionComponent={Transition}>
+                <DialogTitle>Contact Me</DialogTitle>
                 <DialogContent>
                     <DialogContentText>
-                        To subscribe to this website, please enter your email address here. We will send updates
-                        occasionally.
+                        Thanks for taking the time to reach out. How can I help you today?
                     </DialogContentText>
-                    <TextField
-                        autoFocus
-                        margin="dense"
-                        id="name"
-                        label="Email Address"
-                        type="email"
-                        fullWidth
-                    />
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} md={12}>
+                            <TextField
+                                autoFocus
+                                variant="outlined"
+                                label="Full Name"
+                                type="text"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <TextField
+                                variant="outlined"
+                                label="Email Address"
+                                type="email"
+                                fullWidth
+                            />
+                        </Grid>
+                        <Grid item xs={12} md={12}>
+                            <TextField
+                                variant="outlined"
+                                label="Description"
+                                multiline
+                                rows={4}
+                                fullWidth
+                            />
+                        </Grid>
+                    </Grid>
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={isClose} color="primary">
